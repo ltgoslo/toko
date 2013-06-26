aeafcd6159ced1b7c16beb6612b9246f07b99ac9
Murhaf <murhaf.fares@gmail.com>
2013-06-26 11:32:06 +0200

    tokenize.py

---
+++ bin/tokenize.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+older_version = False
+try:
+    from argparse import ArgumentParser
+except ImportError:
+    print "argpase is not available, please ..."
+    older_version = True
+        
+import toko
+import classify
+from classify import classify_file
+import sys
+
+
+def main():
+    if older_version:
+        print "old"
+        classify_file(sys.argv[1])
+    else:
+        classify_file(sys.argv[1])
+        argparser = ArgumentParser(description=__doc__)
+        argparser.add_argument('files', nargs='*',
+                               help='a (list of) file(s) to tokenize')
+        argparser.add_argument('--delimiter', default='\n',
+                               help='specify a delimiter for token boundaries')
+        argparser.add_argument('--model', 
+                               help='path to *Wapiti* model')
+
+        args = argparser.parse_args()
+        
+        pattern = None
+        
+        #for path in args.files:
+        #    process(path, pattern, config)
+        #print args.delimiter
+    
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ except ImportError:
 
 osname = os.uname()[0]
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 if osname == "Linux":
     config = {
         'description': 'PTB Tokenization',
-        long_description=readme(),
+        'long_description': readme(),
         'author': 'murhaff',
         'url': 'https://github.com/Murhaf/toko',
         'download_url': 'https://github.com/Murhaf/toko',
@@ -19,8 +23,11 @@ if osname == "Linux":
         'version': '0.1.0',
         #'install_requires': ['argparse'],
         'packages': find_packages(),
-        'scripts': [],
+        'scripts': ['bin/tokenize.py'],
         'name': 'toko'
+        #entry_points = {
+        #    'console_scripts': ['funniest-joke=funniest.command_line:main'],
+        #    }
         }
 
 
